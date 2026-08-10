cf7be46960041a3b5605510df6ac62e56b66e4d3
snds <jaunebleumarine@gmail.com>
2026-08-10 14:46:34 +0100

    changed eslint config for json files

--- srcs/reqs/node/eslint.config.mjs
+++ srcs/reqs/node/eslint.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json"},
 ]);
