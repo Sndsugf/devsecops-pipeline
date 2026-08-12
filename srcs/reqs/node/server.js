61d287279060455cf5af3e3b0e7be9410e0990c2
snds <jaunebleumarine@gmail.com>
2026-08-12 21:00:08 +0100

    codeql test sql injection

--- srcs/reqs/node/server.js
+++ srcs/reqs/node/server.js
@@ -70,17 +70,19 @@ app.get('/api/items', async (req, res) => {
 
 // Creation d'un item
 app.post('/api/items', async (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({ error: 'Le champ "name" est requis' });
-  }
-  try {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res.status(400).json({ error: 'Le champ "name" est requis' });
+//   }
+//   try {
     // const result = await pool.query(
     //   'INSERT INTO items (name) VALUES ($1) RETURNING id, name, created_at',
     //   [name]
     // );
+	const name = req.body.name;
+	try {
     const result = await pool.query(
-      "INSERT INTO items (name) VALUES ('" + [name] + "') RETURNING id, name, created_at"
+      "INSERT INTO items (name) VALUES ('" + name + "') RETURNING id, name, created_at"
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
